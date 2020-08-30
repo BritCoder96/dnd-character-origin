a414daea0bbee6f18a30c8d26e9a9c3fb6f57982
Janna Pierre <gojanna@aol.com>
2020-08-30 18:00:37 -0400

    Updated test for index

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,10 @@
 import pytest
 import requests
 
+
 url = 'http://0.0.0.0:5000'
 
-def test_api_ocr_page():
-    r = requests.get(url + '/api/ocr')
+
+def test_index():
+    r = requests.get(url + '/')
     assert r.status_code == 200
